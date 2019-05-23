f4dd30420ae64ac455df0f191f485de3dd394f00
DangerN <cdneal091@gmail.com>
2019-05-23 09:39:54 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  let newState
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      newState = {friends: [...state.friends, action.friend]}
+      break
+    case 'REMOVE_FRIEND':
+      newState = {friends: state.friends.filter(fren => fren.id !== action.id)}
+      break
+    default:
+      newState = state
+      break
+  }
+  console.log(newState);
+  return newState
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,12 @@
 export function managePresents(state, action){
+  let newState
+  switch (action.type) {
+    case 'INCREASE':
+      newState = {numberOfPresents: state.numberOfPresents+1}
+      break
+    default:
+      newState = state
+      break
+  }
+  return newState
 }
